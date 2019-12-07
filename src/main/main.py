9ffc630b84b71954c07f0efd7eba2513f8452055
Matthew (Qilong) Chen <qchen1@haverford.edu>
2019-12-07 13:16:42 -0500

    Implement K-means Algorithm

--- src/main/main.py
+++ src/main/main.py
@@ -6,49 +6,50 @@ import csv
 class Algorithm:
 
     def __init__(self):
-        self.graph = {'A': ['B'],
-                      'B': ['C', 'F', 'G'],
-                      'C': ['B', 'D'],
-                      'D': ['C', 'H'],
-                      'E': ['F'],
-                      'F': ['B', 'E', 'H'],
-                      'G': ['B'],
+        self.graph = {'A': ['B', 'G'],
+                      'B': ['C', 'E', 'F', 'G'],
+                      'C': ['B', 'D', 'H'],
+                      'D': ['C', 'H', 'I'],
+                      'E': ['F', 'B', 'G'],
+                      'F': ['B', 'E', 'H', 'J', 'I'],
+                      'G': ['B', 'A', 'E'],
                       'H': ['D', 'F', 'I'],
-                      'I': ['H'],
-                      'J': ['F', 'O', 'M'],
-                      'K': ['M'],
-                      'L': ['M', 'P'],
-                      'M': ['J', 'K', 'L', 'N', 'T', 'U'],
-                      'N': ['O', 'M'],
+                      'I': ['H', 'D', 'F'],
+                      'J': ['F', 'O', 'M', 'N', 'K'],
+                      'K': ['M', 'J', 'L'],
+                      'L': ['M', 'P', 'K'],
+                      'M': ['J', 'K', 'L', 'N', 'T'],
+                      'N': ['O', 'M', 'J'],
                       'O': ['J', 'N'],
                       'P': ['L'],
                       'Q': ['R', 'X'],
-                      'R': ['Q', 'X'],
-                      'S': ['Y'],
+                      'R': ['Q', 'X', 'W', 'Y'],
+                      'S': ['Y', 'U', 'V'],
                       'T': ['M', 'R', 'Y'],
-                      'U': ['M', 'V'],
-                      'V': ['U', 'Z'],
-                      'W': ['Y'],
+                      'U': ['S', 'V'],
+                      'V': ['S', 'U', 'Z'],
+                      'W': ['Y', 'R'],
                       'X': ['Q', 'R'],
-                      'Y': ['T', 'S', 'W', 'Z'],
+                      'Y': ['T', 'S', 'W', 'Z', 'R'],
                       'Z': ['V', 'Y']
                       }
-        self.A = None
-        self.D = None
-        self.L = None
+        self.A = self.getAdjacencyMatrix()
+        self.D = self.getDegreeMatrix()
+        self.L = self.getLaplacianMatrix()
+        self.eigenVals, self.eigenVecs = self.getEigenThings()
 
     def getAdjacencyMatrix(self):
         A = np.zeros((26, 26), dtype=np.int16)
         for key, value in self.graph.items():
-            i = ord(key) - ord('A')
+            row = ord(key) - ord('A')
             for item in value:
-                j = ord(item) - ord('A')
-                A[i, j] = 1
+                column = ord(item) - ord('A')
+                A[row, column] = 1
         with open('AdjacencyMatrix.csv', mode='w') as adjacencyM_file:
             writer = csv.writer(adjacencyM_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for array in A:
                 writer.writerow(array)
-        self.A = A
+        return A
 
     def getDegreeMatrix(self):
         D = np.diag(self.A.sum(axis=1))
@@ -56,7 +57,7 @@ class Algorithm:
             writer = csv.writer(degreeM_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for array in D:
                 writer.writerow(array)
-        self.D = D
+        return D
 
     def getLaplacianMatrix(self):
         L = self.D - self.A
@@ -64,11 +65,27 @@ class Algorithm:
             writer = csv.writer(LaplacianM_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for array in L:
                 writer.writerow(array)
-        self.L = L
+        return L
+
+    def getEigenThings(self):
+        vals, vecs = np.linalg.eig(self.L)
+        vecs = vecs[:, np.argsort(vals)]
+        vals = vals[np.argsort(vals)]
+        return vals, vecs
+
+    def k_means(self, clusters):
+        kmeans = KMeans(n_clusters=clusters)
+        kmeans.fit(self.eigenVecs[:, 1:clusters])
+        colors = kmeans.labels_
+        return colors
 
 
 if __name__ == '__main__':
     solution = Algorithm()
-    solution.getAdjacencyMatrix()
-    solution.getDegreeMatrix()
-    solution.getLaplacianMatrix()
+    vals = solution.eigenVals
+    print(vals)
+    k_mean = solution.k_means(3)
+    count = {0: [], 1: [], 2: []}
+    for i in range(len(k_mean)):
+        count[k_mean[i]].append(chr(i + ord('A')))
+    print(count)
